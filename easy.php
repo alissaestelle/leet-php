3ba091dbad6b1b9010183f1b3441b611024b44e0
Alissa Wiley <alissa.estelle.w@gmail.com>
2023-03-31 15:38:14 -0400

    Prompted Commit

--- easy.php
+++ easy.php
@@ -67,38 +67,93 @@ function twoSum($nums, $target) {
 
 // GREATEST COMMON DIVISOR OF STRINGS
 
-function countChars($elem) {
+function countChars($strA, $strB) {
+  // String is passed through as $elem
 
-  $chars = [];
-  $words = [];
+  // str_split(): splits a word down into letters and places each letter into an array
+  $splitA = str_split($strA);
+  $splitB = str_split($strB);
+  // $arr = $split;
 
-  $split = str_split($elem);
-  $arr = $split;
+  // $matchCheck = array_intersect($splitA, $splitB);
+  // print "Match Check: \n";
+  // print_r($matchCheck);
 
-  $length = count($arr);
-  $last = $length - 1;
+  function compareArrs($arr) {
 
-  for($i = 0; $i < $length; $i++) {
+    $words = [];
+    $chars = [];
 
-    if ($i === 0) array_push($chars, $arr[0]);
-    if ($i === $last)
-    {
-      array_push($chars, $arr[$i]);
-      $group = implode("", $chars);
-      array_push($words, $group);
-      $chars = [];
+    // count(): counts all elements (aka letters) in the array
+    $length = count($arr);
+    $last = $length - 1;
+
+    // A for loop that iterates through each letter in the current array to recognize potential word patterns:
+    for($i = 0; $i < $length; $i++) {
+
+      // If the current iteration is zero, push the first letter into a 2nd (empty) array ($chars):
+      if ($i === 0) array_push($chars, $arr[0]);
+
+      // If the current iteration is the final instance ...
+      if ($i === $last)
+      {
+        // ... push the last letter into the given array ($chars):
+        array_push($chars, $arr[$i]);
+
+        // ... then convert that letter (plus any others in the array) back into a string/word ($group):
+        $group = implode("", $chars);
+
+        // ... then push the new word into a 3rd array ($words):
+        array_push($words, $group);
+
+        // Reset the 2nd array
+        $chars = [];
+      }
+      
+      // If it's not the final iteration AND the current letter *doesn't* match the first letter, then push the current letter into the 2nd array ($chars):
+      if ($i !== $last && $arr[$i] !== $arr[0]) array_push($chars, $arr[$i]);
+
+      // If it's not the first iteration AND the current letter *does* match the first letter ...
+      if ($i !== 0 && $arr[$i] === $arr[0]) 
+      {
+        // ... combine any existing letters in the 2nd array ($chars) into a string/word:
+        $group = implode("", $chars);
+
+        // ... the push that string/word into the 3rd array ($words):
+        array_push($words, $group);
+
+        // Reset the 2nd array ...
+        $chars = [];
+
+        // ... then push the current letter into the 2nd (empty) array ($chars):
+        array_push($chars, $arr[$i]);
+      }
     }
-
-    if ($i !== $last && $arr[$i] !== $arr[0]) array_push($chars, $arr[$i]);
-    if ($i !== 0 && $arr[$i] === $arr[0]) 
-    {
-      $group = implode("", $chars);
-      array_push($words, $group);
-      $chars = [];
-      array_push($chars, $arr[$i]);
-    }
+    return $words;
   }
-  return $words;
+
+  $resultA = compareArrs($splitA);
+  print "1st Array: \n";
+  print_r($resultA);
+  print "1st Array 1st Index: \n";
+  print_r($resultA[0]);
+  print "\n";
+  $resultB = compareArrs($splitB);
+  print_r($resultB);
+
+  $countA = count($resultA);
+  print "$countA \n \n";
+  $countB = count($resultB);
+  print "$countB \n \n";
+
+  if ($resultA[0] !== $resultB[0]) return "";
+
+  $modulo = $countA % $countB;
+
+  $prime = implode("", array($resultB[0]));
+  $GCD = implode("", $resultB);
+
+  return $modulo === 0 ? $GCD : $prime;
 }
 
 $abcabc = "ABCABC";
@@ -113,25 +168,16 @@ $tauxx = "TAUXXTAUXXTAUXXTAUXXTAUXX";
 $leet = "LEET";
 $code = "CODE";
 
-// $solution1 = countChars($abcabc);
-// print_r($solution1);
-// $solution2 = countChars($abc);
-// print_r($solution2);
+// $solution = countChars($abcabc, $abc);
+// print $solution;
 
+// $solution = countChars($abab, $ab);
+// print $solution;
 
-// $solution1 = countChars($abab);
-// print_r($solution1);
-// $solution2 = countChars($ab);
-// print_r($solution2);
+// $solution = countChars($tauxxtauxx, $tauxx);
+// print $solution;
 
-// $solution1 = countChars($tauxxtauxx);
-// print_r($solution1);
-// $solution2 = countChars($tauxx);
-// print_r($solution2);
-
-$solution1 = countChars($leet);
-print_r($solution1);
-$solution2 = countChars($code);
-print_r($solution2);
+$solution = countChars($leet, $code);
+print $solution;
 
 ?>
--- solutions.php
+++ solutions.php
@@ -12,8 +12,6 @@
 $arr = [2, 5, 5, 11];
 $sum = 10;
 
-// 
-
 function twoSum($nums, $target) {
   $length = count($nums);
   $new = [];
@@ -34,5 +32,86 @@ function twoSum($nums, $target) {
 
 // $ans = twoSum($arr, $sum);
 // print_r($ans);
+
+
+// GREATEST COMMON DIVISOR OF STRINGS
+
+function countChars($strA, $strB) {
+
+  $splitA = str_split($strA);
+  $splitB = str_split($strB);
+
+  function compareArrs($arr) {
+
+    $words = [];
+    $chars = [];
+
+    $length = count($arr);
+    $last = $length - 1;
+
+    for($i = 0; $i < $length; $i++) {
+
+      if ($i === 0) array_push($chars, $arr[0]);
+
+      if ($i === $last)
+      {
+        array_push($chars, $arr[$i]);
+        $group = implode("", $chars);
+        array_push($words, $group);
+        $chars = [];
+      }
+      
+      if ($i !== $last && $arr[$i] !== $arr[0]) array_push($chars, $arr[$i]);
+
+      if ($i !== 0 && $arr[$i] === $arr[0]) 
+      {
+        $group = implode("", $chars);
+        array_push($words, $group);
+        $chars = [];
+        array_push($chars, $arr[$i]);
+      }
+    }
+    return $words;
+  }
+
+  $resultA = compareArrs($splitA);
+  $resultB = compareArrs($splitB);
+
+  $countA = count($resultA);
+  $countB = count($resultB);
+
+  if ($resultA[0] !== $resultB[0]) return "";
+
+  $modulo = $countA % $countB;
+
+  $prime = implode("", array($resultB[0]));
+  $GCD = implode("", $resultB);
+
+  return $modulo === 0 ? $GCD : $prime;
+}
+
+$abcabc = "ABCABC";
+$abc = "ABC";
+
+$abab = "ABABAB";
+$ab = "ABAB";
+
+$tauxxtauxx = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX";
+$tauxx = "TAUXXTAUXXTAUXXTAUXXTAUXX";
+
+$leet = "LEET";
+$code = "CODE";
+
+// $solution = countChars($abcabc, $abc);
+// print $solution;
+
+// $solution = countChars($abab, $ab);
+// print $solution;
+
+// $solution = countChars($tauxxtauxx, $tauxx);
+// print $solution;
+
+$solution = countChars($leet, $code);
+print $solution;
 
 ?>
